839f51e4506d2f3e02ac1b2374e0790a48ed23a2
Aditya Raj <adityaraj@Adityas-MacBook-Air.local>
2026-07-14 22:27:26 +0530

    update

--- esportIndia/src/environment/environment.ts
+++ esportIndia/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
 //   apiUrl: 'http://localhost:8080/esportsbuzz',
-    apiUrl: 'https://esportsbuzz.vercel.app/',
+    apiUrl: 'https://esportsbuzz.vercel.app/esportsbuzz',
 
 };
