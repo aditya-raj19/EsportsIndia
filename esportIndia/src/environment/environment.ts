7fc5969a23340847ca08d6e14194821389887bb1
Aditya Raj <adityaraj@Adityas-MacBook-Air.local>
2026-07-14 22:25:27 +0530

    changed packing to jar

--- esportIndia/src/environment/environment.ts
+++ esportIndia/src/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/esportsbuzz',
-  
+//   apiUrl: 'http://localhost:8080/esportsbuzz',
+    apiUrl: 'https://esportsbuzz.vercel.app/',
+
 };
